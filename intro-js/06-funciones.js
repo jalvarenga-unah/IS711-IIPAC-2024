4b901435d5717340dbc3aa14fe200c37c6b90db1
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-17 22:25:58 -0600

    desestructuracion en funciones

--- intro-js/06-funciones.js
+++ intro-js/06-funciones.js
@@ -5,8 +5,6 @@ const exec = {
 }
 
 function saludar(nombre, apellido) {
-
-
     // console.log("Hola mundo");
     return `Hola ${nombre} ${apellido}`;
 
@@ -17,6 +15,14 @@ let sumar = function (a, b) {
     return a - b
 }
 
+const dividir = ({ denominador, numerador }) => {
+
+    if (denominador === 0) {
+        throw 'No se puede dividir por cero'
+    }
+    return numerador / denominador
+}
+
 const test = () => {
     console.log('Hola mundo');
 }
@@ -25,9 +31,20 @@ const contenido = saludar('Alvarenga', 'Juan');
 
 // saludar() = "Hola mundo2"; // ❌ No se puede reasignar
 
+try {
+    console.log(dividir({ denominador: 0, numerador: 3 }));
+} catch {
+    alert('kiapasaoo')
+}
 console.log(contenido);
 console.log(saludar('Pedro'));
 console.log(saludar(124));
 console.log(exec['test']());
 
 console.log(sumar(3, '2'));
+
+
+const datos = { b: 2, a: 3, c: 5, rating: { rate: 124, count: 235 } }
+const { count, rate } = { rate: 2.3, count: 100 };
+
+
