cfd720cf849ea8744309de7cb06514eb38ed3d4e
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-06-19 07:34:40 -0600

    ejercicio en clase

--- intro-js/06-funciones.js
+++ intro-js/06-funciones.js
@@ -34,7 +34,7 @@ const contenido = saludar('Alvarenga', 'Juan');
 try {
     console.log(dividir({ denominador: 0, numerador: 3 }));
 } catch {
-    alert('kiapasaoo')
+    // alert('kiapasaoo')
 }
 console.log(contenido);
 console.log(saludar('Pedro'));
@@ -48,3 +48,18 @@ const datos = { b: 2, a: 3, c: 5, rating: { rate: 124, count: 235 } }
 const { count, rate } = { rate: 2.3, count: 100 };
 
 
+const miniPrueba = [3, 6, 4, 9, 5, 6, 7, 9, 2]
+
+const sumaAdyacente = (numeros) => {
+    let sumaAcumulada = 0;
+
+    for (let i = 0; i < numeros.length - 1; i++) {
+        const suma = numeros[i] + numeros[i + 1]
+        if (suma > sumaAcumulada) {
+            sumaAcumulada = suma
+        }
+    }
+    return sumaAcumulada
+}
+
+console.log(sumaAdyacente(miniPrueba))
