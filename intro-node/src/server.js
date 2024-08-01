aedc7f8f46ba876114348715ea83048f6bb11c44
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2024-07-31 20:53:46 -0600

    Creando una API con express

--- intro-node/src/server.js
+++ intro-node/src/server.js
@@ -1,1 +1,37 @@
-// nuestro servidor WEB
+const express = require('express');
+
+// Creación del app de express
+const app = express() // instancia de express
+app.disable('x-powered-by') // deshabilitar la cabecera x-powered-by
+
+//establecer el puerto
+const PORT = process.env.PORT || 3000
+
+
+//RUTAS
+
+app.get('/', (request, response) => {
+
+    response.send('<h1>Mi primer API</h1>')
+
+})
+
+app.get('/saludo/:name', (req, res) => {
+
+
+    // const nombre = req.params.name
+    const { name } = req.params
+
+    // res.send(`<h1>Hola ${name}</h1>`)
+    //TODO: devoler codigo y encabezado correcto
+    res.json({ message: `Hola ${name}` })
+
+})
+
+
+
+
+// lo poner en marcha
+app.listen(PORT, () => {
+    console.log(`El servidor se ejecuta en: http://localhost:${PORT}`)
+})
